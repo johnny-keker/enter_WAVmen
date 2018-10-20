f8d431d082e8ff96c26a9776bc0881c9f51125e3
Johnny Kekker <giomail.iv@gmail.com>
2018-10-20 20:32:23 +0300

    notes library wip

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 extern crate byteorder;
 extern crate rand;
 
+#[allow(dead_code)]
+mod notes;
+
 use rand::prelude::*;
 use std::fs::File;
 use std::io::prelude::*;
 use std::env;
 
 fn main() {
+  print!("{:?}", notes::A0);
   let args: Vec<String> = env::args().collect();
   let filename = &args[1];
-  let wav: Vec<u8> = generate_wav(44100, 20, 120).unwrap();
+  let wav: Vec<u8> = generate_wav(44100, 40, 120).unwrap();
   let mut file = File::create(format!("out/{}.wav", filename)).unwrap();
   file.write(&wav).unwrap();
 }
